9cd80eff50a2a47bbbedd2cc438c808ba3362722
Jonas Larsen <okjonas@gmail.com>
2018-10-31 22:55:46 +0100

    Settings and profile screen done

--- lib/src/screens/settings_screen.dart
+++ lib/src/screens/settings_screen.dart
@@ -1,19 +1,90 @@
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return SettingsScreenState();
+  }
+}
+
+class SettingsScreenState extends State<SettingsScreen> {
+  bool clockConvention = false;
+  bool notificationSound = false;
+  bool notificationOn = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
         title: Text('Settings screen'),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //
-          },
-          child: Text('Settings Screen'),
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(15.0),
+            child: Text(
+              'General',
+              style: TextStyle(color: Colors.blue, fontSize: 15.0),
+            ),
+          ),
+          ListTile(
+            title: Text('24-hour clock'),
+            trailing: Switch(
+              value: clockConvention,
+              onChanged: (value) {
+                print('${value}');
+                setState(() {
+                  if (clockConvention == true) {
+                    clockConvention = false;
+                  } else {
+                    clockConvention = true;
+                  }
+                });
+              },
+            ),
+          ),
+          Divider(),
+          Container(
+            padding: EdgeInsets.all(15.0),
+            child: Text(
+              'Notifications',
+              style: TextStyle(color: Colors.blue, fontSize: 15.0),
+            ),
+          ),
+          ListTile(
+            title: Text('Notify on this device'),
+            trailing: Switch(
+              value: notificationOn,
+              onChanged: (value) {
+                print('${value}');
+                setState(() {
+                  if (notificationOn == true) {
+                    notificationOn = false;
+                  } else {
+                    notificationOn = true;
+                  }
+                });
+              },
+            ),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Sound'),
+            trailing: Switch(
+              value: notificationSound,
+              onChanged: (value) {
+                print('${value}');
+                setState(() {
+                  if (notificationSound == true) {
+                    notificationSound = false;
+                  } else {
+                    notificationSound = true;
+                  }
+                });
+              },
+            ),
+          )
+        ],
       ),
     );
   }
