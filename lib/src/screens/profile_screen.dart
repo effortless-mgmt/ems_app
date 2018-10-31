1b8d1ecd5469fb8e4410ed83e20929af0fcf9c1e
Okjonas <okjonas@gmail.com>
2018-11-01 00:45:13 +0100

    Profile page and settings (#21)
    * Started on profile screen.
    Created a settings icon in the appbar.
    Created settings_screen.dart.
    
    * Navigation to setting screen fixed
    
    * Settings and profile screen done

--- lib/src/screens/profile_screen.dart
+++ lib/src/screens/profile_screen.dart
@@ -4,14 +4,57 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //
-          },
-          child: Text('Profile screen'),
+        body: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(20.0),
         ),
-      ),
-    );
+        CircleAvatar(
+          backgroundImage: NetworkImage(
+              'https://thediplomat.com/wp-content/uploads/2017/10/thediplomat-ap_923638204453-386x261.jpg'),
+          radius: 80.0,
+        ),
+        Container(
+          height: 20.0,
+        ),
+        Card(
+          child: Column(
+            children: <Widget>[
+              const ListTile(
+                //trailing: Text('data'),
+                leading: Icon(Icons.portrait),
+                title: Text('Kim Jong Un'),
+                subtitle: Text('Name'),
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.map),
+                title: Text('조선민주주의인민공화국'),
+                subtitle: Text('Address'),
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.phone),
+                title: Text('+850 2 18111'),
+                subtitle: Text('Phone'),
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.email),
+                title: Text('김정은@김.은'),
+                subtitle: Text('Email'),
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.local_atm),
+                title: Text('None'),
+                subtitle: Text('Tax Card'),
+              )
+            ],
+          ),
+        ),
+      ],
+    ));
   }
 }
