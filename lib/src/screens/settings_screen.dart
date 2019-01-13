c1541558fb729d488e2a103439d06dadfaf3f867
Jonas Larsen <okjonas@gmail.com>
2019-01-13 21:33:51 +0100

    Theme not done

--- lib/src/screens/settings_screen.dart
+++ lib/src/screens/settings_screen.dart
@@ -1,4 +1,6 @@
+import 'package:ems_app/src/screens/settings/theme_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsScreen extends StatefulWidget {
   @override
@@ -8,84 +10,161 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class SettingsScreenState extends State<SettingsScreen> {
-  bool clockConvention = false;
-  bool notificationSound = false;
-  bool notificationOn = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
         title: Text('Settings screen'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(15.0),
-            child: Text(
-              'General',
-              style: TextStyle(color: Colors.blue, fontSize: 15.0),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                'Account',
+                style: TextStyle(color: Colors.blue, fontSize: 15.0),
+              ),
             ),
-          ),
-          ListTile(
-            title: Text('24-hour clock'),
-            trailing: Switch(
-              value: clockConvention,
-              onChanged: (value) {
-                print('$value');
-                setState(() {
-                  if (clockConvention == true) {
-                    clockConvention = false;
-                  } else {
-                    clockConvention = true;
-                  }
-                });
+            ListTile(
+              onTap: () {
+                print("YOLO");
+              },
+              title: Text('Change password'),
+            ),
+            ListTile(
+              onTap: _showDialog,
+              title: Text('Log out'),
+            ),
+            Divider(),
+            Container(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                'App settings',
+                style: TextStyle(color: Colors.blue, fontSize: 15.0),
+              ),
+            ),
+            ListTile(
+              title: Text('Theme'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ThemeScreen()),
+                );
               },
             ),
-          ),
-          Divider(),
-          Container(
-            padding: EdgeInsets.all(15.0),
-            child: Text(
-              'Notifications',
-              style: TextStyle(color: Colors.blue, fontSize: 15.0),
-            ),
-          ),
-          ListTile(
-            title: Text('Notify on this device'),
-            trailing: Switch(
-              value: notificationOn,
-              onChanged: (value) {
-                print('$value');
-                setState(() {
-                  if (notificationOn == true) {
-                    notificationOn = false;
-                  } else {
-                    notificationOn = true;
-                  }
-                });
+            ListTile(
+              title: Text('Calendar'),
+              onTap: () {
+                print("YOLO");
               },
             ),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('Sound'),
-            trailing: Switch(
-              value: notificationSound,
-              onChanged: (value) {
-                print('$value');
-                setState(() {
-                  if (notificationSound == true) {
-                    notificationSound = false;
-                  } else {
-                    notificationSound = true;
-                  }
-                });
+            Divider(),
+            Container(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                'Need Help?',
+                style: TextStyle(color: Colors.blue, fontSize: 15.0),
+              ),
+            ),
+            ListTile(
+              title: Text('FAQ'),
+              onTap: () {
+                print("YOLO");
               },
             ),
-          )
-        ],
+            ListTile(
+              title: Text('Troubleshooting'),
+              onTap: _showDialog,
+            ),
+            ListTile(
+              title: Text('Contact support'),
+              onTap: () {
+                _launchURL(
+                    "mailto:smith@example.org?subject=News&body=New%20plugin");
+              },
+            ),
+            Divider(),
+            Container(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                'About',
+                style: TextStyle(color: Colors.blue, fontSize: 15.0),
+              ),
+            ),
+            ListTile(
+              title: Text('What\'s new'),
+              onTap: () {
+                _launchURL(
+                    "https://www.cbronline.com/news/flutter-1-0-released");
+              },
+            ),
+            ListTile(
+              title: Text('Rate app'),
+              onTap: () {
+                _launchURL(
+                    "https://support.google.com/googleplay/answer/6209544?hl=en");
+              },
+            ),
+            ListTile(
+              title: Text('Terms of use'),
+              onTap: () {
+                _launchURL("https://en.wikipedia.org/wiki/Terms_of_service");
+              },
+            ),
+            ListTile(
+              title: Text('Imprint'),
+              onTap: () {
+                _launchURL(
+                    "https://en.wikipedia.org/wiki/Imprint_(trade_name)");
+              },
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  _launchURL(url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  // user defined function
+  void _showDialog() {
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Are you sure?"),
+          content: new Text(
+              "This will erase all data from this device and take you back to the login screen"),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text(
+                "LOG OUT",
+                style: TextStyle(color: Colors.red),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            new FlatButton(
+              child: new Text("CANCEL"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
