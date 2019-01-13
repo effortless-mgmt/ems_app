0fc93c58d17c448504e7176c641325757be13ec7
tlien <thomaslienchristensen@gmail.com>
2019-01-13 19:55:57 +0100

    Changed to SliverAppBar to allow smoother scrolling. Keeping old code for now. Closes #83

--- lib/src/screens/appointment_details/appointment_details_screen.dart
+++ lib/src/screens/appointment_details/appointment_details_screen.dart
@@ -1,7 +1,9 @@
 import 'package:ems_app/src/models/appointment.dart';
-import 'package:ems_app/src/screens/appointment_details/maps.dart';
+import 'package:ems_app/src/screens/appointment_details/contact_widget.dart';
+import 'package:ems_app/src/screens/appointment_details/maps_widget.dart';
 import 'package:ems_app/src/screens/appointment_details/testRoute.dart';
 import 'package:ems_app/src/screens/appointment_details/appBarDescriptive.dart';
+import 'package:ems_app/util/user_list.dart';
 import 'package:flutter/material.dart';
 
 class AppointmentDetailsScreen extends StatefulWidget {
@@ -13,20 +15,59 @@ class AppointmentDetailsScreen extends StatefulWidget {
   State<StatefulWidget> createState() => new _AppointmentDetailsState();
 }
 
+// SLIVER APP BAR
 class _AppointmentDetailsState extends State<AppointmentDetailsScreen> {
   Appointment _appointment = Appointment.demodata[0];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarDescriptive(
-          title: Text("Next Appointment"), appointment: _appointment),
-      body: Maps(address: _appointment.address),
-      // body: RaisedButton(
-      //     child: Text("Test"),
-      //     onPressed: () => Navigator.push(
-      //         context,
-      //         MaterialPageRoute(
-      //             builder: (context) => AppointmentDetailsScreen2()))));
+      body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              AppBarDescriptive(
+                  title: Text("Next Appointment"), appointment: _appointment)
+            ];
+          },
+          body: Column(
+            children: <Widget>[
+              Maps(address: _appointment.address),
+              Flexible(
+                child: ListView.builder(
+                    primary: true,
+                    itemBuilder: _listBuilder,
+                    itemCount: sampleUserList.userList.length),
+              )
+            ],
+          )),
     );
+  }
+
+  /*########################################### 
+    STANDARD APP BAR IMPLEMENTATION. FUNCTIONAL 
+    ########################################### */
+
+  // class _AppointmentDetailsState extends State<AppointmentDetailsScreen> {
+  // Appointment _appointment = Appointment.demodata[0];
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBarDescriptive(
+  //         title: Text("Next Appointment"), appointment: _appointment),
+  //     body: Column(
+  //       children: <Widget>[
+  //         Maps(address: _appointment.address),
+  //         Flexible(
+  //           child: ListView.builder(
+  //               itemBuilder: _listBuilder,
+  //               itemCount: sampleUserList.userList.length),
+  //         ),
+  //         // ContactListTile(user: sampleUserList.userList[0])
+  //       ],
+  //     ),
+  //   );
+  // }
+
+  Widget _listBuilder(BuildContext context, int index) {
+    return ContactListTile(user: sampleUserList.userList[index]);
   }
 }
