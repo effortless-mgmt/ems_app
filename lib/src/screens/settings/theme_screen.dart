8e6fa044ed9509b216a98f4bebdd90ed9720eb85
Jonas Larsen <okjonas@gmail.com>
2019-01-14 10:40:47 +0100

    Theme settings done.

--- lib/src/screens/settings/theme_screen.dart
+++ lib/src/screens/settings/theme_screen.dart
@@ -4,54 +4,67 @@ class ThemeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Center(
-            child: Container(
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                "Pick a different color scheme",
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Container(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  "Pick a different color scheme",
+                ),
               ),
             ),
-          ),
-          GestureDetector(
-            onTap: () {
-              print("Theme blue is selected");
-            },
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(5),
-              child: Image(
-                image: AssetImage('assets/blue.png'),
-                fit: BoxFit.fill,
+            GestureDetector(
+              onTap: () {
+                print("Theme blue is selected");
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Image(
+                    image: AssetImage('assets/blue.png'),
+                  ),
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 10.0),
-          GestureDetector(
-            onTap: () {
-              print("Theme black is selected");
-            },
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(5),
-              child: Image(
-                image: AssetImage('assets/black.png'),
-                fit: BoxFit.fill,
+            SizedBox(height: 10.0),
+            GestureDetector(
+              onTap: () {
+                print("Theme black is selected");
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Image(
+                    image: AssetImage('assets/black.png'),
+                  ),
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 10.0),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(5),
-            child: Image(
-              image: AssetImage('assets/red.png'),
-              fit: BoxFit.fill,
+            SizedBox(height: 10.0),
+            GestureDetector(
+              onTap: () {
+                print("Red theme selected");
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Image(
+                    image: AssetImage('assets/red.png'),
+                  ),
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       appBar: AppBar(
-        title: Text("Themes "),
+        title: Text("Themes"),
       ),
     );
   }
