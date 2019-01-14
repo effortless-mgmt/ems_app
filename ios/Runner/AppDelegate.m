e144aa54dc5cb4807bd845d7adfd382c3be09e01
algorythm <anders@wiberg.tech>
2019-01-14 12:22:41 +0100

    Fix Google Maps for iOS

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,13 +1,23 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    
+    // Property List with private keys, gitignored.
+    NSDictionary *keyDict = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"secret_keys" ofType:@"plist"]];
+
+    //NSDictionary *keyDict = [NSDictionary dictionaryWithContentsOfFile:@"secret_keys.plist"];
+    printf("This is a test!");
+    
+    
+    [GMSServices provideAPIKey:keyDict[@"google_maps_api_key"]];
+    [GeneratedPluginRegistrant registerWithRegistry:self];
+    // Override point for customization after application launch.
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 @end
