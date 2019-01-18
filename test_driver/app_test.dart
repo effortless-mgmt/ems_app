33117ee98a5e64a8da86e2378e2860fbc6c76a73
Jonas Larsen <okjonas@gmail.com>
2019-01-18 21:32:41 +0100

    Started on integration test

---
+++ test_driver/app_test.dart
@@ -0,0 +1,22 @@
+// Imports the Flutter Driver API
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Counter App', () {
+    FlutterDriver driver;
+    final buttonFinder = find.byValueKey('Login');
+
+    // Connect to the Flutter driver before running any tests
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    // Close the connection to the driver after the tests have completed
+    tearDownAll(() async {
+      if (driver != null) {
+        driver.close();
+      }
+    });
+  });
+}
